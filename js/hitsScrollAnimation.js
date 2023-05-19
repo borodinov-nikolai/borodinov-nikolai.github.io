c496763772fab7263be285fbcd4c9e76e5378136
borodinov-nikolai <127582862+borodinov-nikolai@users.noreply.github.com>
2023-05-19 15:36:32 +0300

    Add files via upload

--- js/hitsScrollAnimation.js
+++ js/hitsScrollAnimation.js
@@ -11,17 +11,19 @@ rightImgHolder.style.marginLeft = '0px';
     window.addEventListener('scroll', function(event) {
 
         if(this.pageYOffset > 240 && this.pageYOffset < 1800 ) {
+            leftImgHolder.style.marginLeft = '-400px';
             leftImgHolder.style.marginLeft = '0px';
+            rightImgHolder.style.marginLeft = '800px';
             rightImgHolder.style.marginLeft = '0px';
-            leftImgHolder.classList.remove('hidden');
-            rightImgHolder.classList.remove('hidden');
+            // leftImgHolder.classList.remove('hidden');
+            // rightImgHolder.classList.remove('hidden');
 
         } else {
             leftImgHolder.style.marginLeft = '-400px';
             rightImgHolder.style.marginLeft = '800px';
 
-            leftImgHolder.classList.add('hidden');
-            rightImgHolder.classList.add('hidden');
+            // leftImgHolder.classList.add('hidden');
+            // rightImgHolder.classList.add('hidden');
         }
     })
 
